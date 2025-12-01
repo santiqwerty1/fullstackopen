e4540892d2ab7c81cb9dfb7cc9ad37158884cfce
Santiago Aguilar <santiago.aguilar995@gmail.com>
2025-12-01 17:49:25 -0300

    excercise 1.1 using just props instead of destructure props

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,6 +1,12 @@
-const Header = ({ course }) => <h1>{course}</h1>
-const Content = ({ part, exercises }) => <p>{part} {exercises}</p>
-const Total = ({ exercises }) => <p>Number of exercises {exercises}</p>
+const Header = (props) => <h1>{props.course}</h1>
+const Content = (props) => (
+  <div>
+    <p>{props.part1} {props.exercises1}</p>
+    <p>{props.part2} {props.exercises2}</p>
+    <p>{props.part3} {props.exercises3}</p>
+  </div>
+)
+const Total = (props) => <p>Number of exercises {props.exercises}</p>
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -13,9 +19,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   )
